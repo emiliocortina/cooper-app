8d7679d094ab602c3c51a3abfc30245327e79935
Emilio <emiliocortina@gmail.com>
2019-10-30 23:39:05 +0100

    Minicards

---
+++ screens/Tabs/Search/Search.stylesheet.js
@@ -0,0 +1,28 @@
+const searchStyles = {
+  Content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexDirection: "column"
+  },
+  Row: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  MiniCard: {
+    height: 100,
+    flex: 1,
+    backgroundColor: "blue",
+    marginHorizontal: 5,
+    borderRadius: 15,
+      marginLeft: 15
+  },
+  Left: {
+    marginLeft: 10
+  },
+  Right: {
+    marginRight: 10
+  }
+};
+
+export default searchStyles;
